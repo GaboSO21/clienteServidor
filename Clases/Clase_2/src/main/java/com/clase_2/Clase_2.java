243a432cc01cb120246706d120f38f007b4e0bf4
GaboSO21 <gso2090@gmail.com>
2022-09-29 18:55:54 -0600

    Herencia

---
+++ Clases/Clase_2/src/main/java/com/clase_2/Clase_2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.clase_2;
+
+/**
+ *
+ * @author GabrielPC
+ */
+public class Clase_2 {
+
+    public static void main(String[] args) {
+        
+
+        
+
+        
+    }
+}
