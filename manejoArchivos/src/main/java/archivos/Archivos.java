bd31d9efe24ef12ddb443c8589df4d66d522825e
GaboSO21 <gso2090@gmail.com>
2022-10-27 14:54:01 -0600

    Archivos

---
+++ manejoArchivos/src/main/java/archivos/Archivos.java
@@ -0,0 +1,63 @@
+package archivos;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Archivos {
+
+    public static void crearArchivo(String nombreArchivo) {
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void escribirArchivo(String nombreArchivo, String contenido) {
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            salida.close();
+            System.out.println("Se ha escrito el archivo");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void anexarInformacion(String nombreArchivo, String contenido) {
+        File archivo = new File(nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter(new FileWriter(archivo, true));
+            salida.println(contenido);
+            salida.close();
+            System.out.println("Se ha anexado el archivo");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void leerArchivo(String nombreArchivo) {
+        File archivo = new File(nombreArchivo);
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+            String lectura = entrada.readLine();
+            while (lectura != null) {
+                System.out.println(lectura);
+                lectura = entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
---
+++ manejoArchivos/src/main/java/main/ManejoArchivos.java
@@ -0,0 +1,14 @@
+
+
+package main;
+import static archivos.Archivos.*;
+
+public class ManejoArchivos {
+
+    public static void main(String[] args) {
+        var nombre = "prueba.txt";
+        // crearArchivo(nombre);
+
+        leerArchivo(nombre);
+    }
+}
