4074fbf8e3ffee3906f09af40fa0dc83dfe4d207
GaboSO21 <gso2090@gmail.com>
2022-11-10 16:55:53 -0600

    JDBC

--- IntroJDBC/src/main/java/main/IntroJDBC.java
+++ IntroJDBC/src/main/java/main/IntroJDBC.java
@@ -15,27 +15,27 @@ public class IntroJDBC {
 
     public static void main(String[] args) {
         
-        var url = "jdbc:mysql://localhost:3306/test?";
-        try {
-            // Class.forName("com.mysql.cj.jdb.Driver");
-            Connection conexion = DriverManager.getConnection(url, "root", "admin");
-            Statement instruccion = conexion.createStatement();
-            var sql = "SELECT id_Persona, nombre, apellido, email, telefono FROM persona";
-            ResultSet resultado = instruccion.executeQuery(sql);
+        // var url = "jdbc:mysql://localhost:3306/test?";
+        // try {
+        //     // Class.forName("com.mysql.cj.jdb.Driver");
+        //     Connection conexion = DriverManager.getConnection(url, "root", "admin");
+        //     Statement instruccion = conexion.createStatement();
+        //     var sql = "SELECT id_Persona, nombre, apellido, email, telefono FROM persona";
+        //     ResultSet resultado = instruccion.executeQuery(sql);
     
-            while (resultado.next()) {
-                System.out.print("ID Persona: " + resultado.getInt("id_Persona"));
-                System.out.print(" Nombre: " + resultado.getString("nombre"));
-                System.out.print(" Apellido: " + resultado.getString("apellido"));
-                System.out.print(" Email: " + resultado.getString("email"));
-                System.out.println(" Telefono: " + resultado.getString("telefono"));
-            }
-            resultado.close();
-            instruccion.close();
-            conexion.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        //     while (resultado.next()) {
+        //         System.out.print("ID Persona: " + resultado.getInt("id_Persona"));
+        //         System.out.print(" Nombre: " + resultado.getString("nombre"));
+        //         System.out.print(" Apellido: " + resultado.getString("apellido"));
+        //         System.out.print(" Email: " + resultado.getString("email"));
+        //         System.out.println(" Telefono: " + resultado.getString("telefono"));
+        //     }
+        //     resultado.close();
+        //     instruccion.close();
+        //     conexion.close();
+        // } catch (SQLException e) {
+        //     e.printStackTrace();
+        // }
 
     }
 }
